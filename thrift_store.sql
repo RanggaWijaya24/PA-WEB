c3962e2788d8bad91edbdaf6ff526faf1d44cd47
Biane3006 <Sarulaja300603@gmail.com>
2022-11-14 23:31:53 +0800

    Update Database

--- thrift_store.sql
+++ thrift_store.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 13, 2022 at 06:04 AM
+-- Generation Time: Nov 14, 2022 at 04:30 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.2
 
@@ -43,7 +43,8 @@ CREATE TABLE `akun` (
 INSERT INTO `akun` (`Id_Akun`, `Username`, `Password`, `Email`, `Role`) VALUES
 (1, 'admin', '$2y$10$z12UIYeGsvBr6gglhK8jQu9zcox.4r3AdPUPBFUM36I8y1HyXEcZ6', 'admin@gmail.com', 'admin'),
 (7, 'user1', '$2y$10$z12UIYeGsvBr6gglhK8jQu9zcox.4r3AdPUPBFUM36I8y1HyXEcZ6', 'user@gmail.com', 'user'),
-(8, 'user2', '$2y$10$8rbYW.dUoqZVIC8hGH4yOuM4j2Sq3.nLHWPFlbdvkiSqJagEdRY3O', 'user2@gmail.com', 'user');
+(8, 'user2', '$2y$10$8rbYW.dUoqZVIC8hGH4yOuM4j2Sq3.nLHWPFlbdvkiSqJagEdRY3O', 'user2@gmail.com', 'user'),
+(9, 'user3', '$2y$10$XXD5GRxZxwE3UOx17TDkrujcRO71IY5WGvK1Ki06822mqdluMTO4S', 'user3@gmail.com', 'user');
 
 -- --------------------------------------------------------
 
@@ -63,7 +64,7 @@ CREATE TABLE `cart` (
 --
 
 INSERT INTO `cart` (`Id_Akun`, `Id_Produk`, `Quantity`, `Total_Harga`) VALUES
-(8, 25, 3, 360000);
+(7, 25, 15, 1800000);
 
 -- --------------------------------------------------------
 
@@ -84,7 +85,8 @@ CREATE TABLE `contact` (
 --
 
 INSERT INTO `contact` (`Id_Akun`, `Nama`, `Email_Pengirim`, `Subject`, `Pesan`) VALUES
-(NULL, 'Syamsir', 'Syamsir300603@gmail.com', 'Kesehatan', 'Jangan Lupa Tidur');
+(NULL, 'Syamsir', 'Syamsir300603@gmail.com', 'Kesehatan', 'Jangan Lupa Tidur'),
+(8, 'Arif', 'arifaja@gmail.com', 'Mabarkah', 'Ayok');
 
 -- --------------------------------------------------------
 
@@ -106,9 +108,9 @@ CREATE TABLE `produk` (
 --
 
 INSERT INTO `produk` (`Id_Produk`, `Nama_Produk`, `Gambar`, `Harga`, `Sisa_Stok`, `Tanggal`) VALUES
-(23, 'Kemeja Hitam', 'df54161b14688f9933e61478d7eeb367-n8.jpg', 50000, 47, '2022-11-12 14:46:17'),
-(24, 'Lakers', 'cd1145b85def00144a6d9ef456548fc5-lakers-removebg-preview.png', 45000, 47, '2022-11-12 16:01:10'),
-(25, 'Kemeja', '48e28a575d3e75fb46549f4036059620-n5.jpg', 120000, 44, '2022-11-12 15:58:28');
+(23, 'Kemeja Hitam', 'df54161b14688f9933e61478d7eeb367-n8.jpg', 50000, 16, '2022-11-12 14:46:17'),
+(25, 'Kemeja', '616af73c826952af02bb579f672622e5-n5.jpg', 120000, 55, '2022-11-13 14:39:44'),
+(27, 'Baju Pantai', 'bc068ba3d0712e80284b35120116046c-f2.jpg', 60000, 35, '2022-11-14 17:01:42');
 
 --
 -- Indexes for dumped tables
@@ -147,13 +149,13 @@ ALTER TABLE `produk`
 -- AUTO_INCREMENT for table `akun`
 --
 ALTER TABLE `akun`
-  MODIFY `Id_Akun` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id_Akun` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `produk`
 --
 ALTER TABLE `produk`
-  MODIFY `Id_Produk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `Id_Produk` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
